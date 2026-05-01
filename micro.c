309c764d3be3fc44cc8f7566a840cf1cc757737a
DIEGO <diegoquinter3@gmail.com>
2026-04-30 19:35:15 -0500

    first commit

---
+++ micro.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a, b;
+     printf("Ingrese el primer numero: ");
+    scanf("%d", &a);
+
+    printf("Ingrese el segundo numero: ");
+    scanf("%d", &b);
+    
+}
